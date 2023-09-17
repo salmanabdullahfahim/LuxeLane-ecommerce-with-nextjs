6aaa08b68fbda87f93742a62a7c697a163334a88
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-17 21:50:35 +0600

    google  provider and credentials added in the auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,5 +3,36 @@ import GoogleProvider from "next-auth/providers/google";
 import CredentialProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Enter your username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "101", username: "test", password: "test123" };
+        if (
+          credentials?.username === user.username &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className=" text-2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, nam?
-      </h1>
+      <h1 className=" text-2xl"></h1>
     </main>
   );
 }
