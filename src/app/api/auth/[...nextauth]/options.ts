272152dca0bce55d32a1ea586d65715ccfa6a4a0
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-17 20:57:46 +0600

    initial nextauth setup for authentication - done

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,7 @@
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [],
+};
