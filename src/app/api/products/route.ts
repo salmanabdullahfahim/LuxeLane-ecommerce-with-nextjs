e63d489f47e0ac265f9e392a61fedbdd2bd325ec
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-19 11:42:56 +0600

    all products api created

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,3 +1,21 @@
 import { NextResponse } from "next/server";
+import { productData } from "@/constants/data";
 
-export const GET = async (req: Request, res: NextResponse) => {};
+export const GET = async () => {
+  try {
+    return NextResponse.json({
+      message: "Product data get successfully",
+      success: true,
+      productData,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Product is not available",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
