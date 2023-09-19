ad6733cc892e4e706d8cdc0cc379ad49980102e2
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-19 12:49:08 +0600

    all product image rendered

--- src/components/ProductData.tsx
+++ src/components/ProductData.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { ProductProps } from "../../type";
+import Image from "next/image";
 
-const ProductData = () => {
-  return <div>ProductData</div>;
+const ProductData = ({ product }: ProductProps) => {
+  return (
+    <div className="w-full rounded-lg overflow-hidden">
+      <div>
+        <div className="w-full h-96 overflow-hidden relative group">
+          <Image
+            src={product?.image}
+            alt="product image"
+            width={500}
+            height={500}
+            className="w-full h-full object-cover group-hover:scale-110 duration-200 rounded-t-lg"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductData;
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,14 +1,15 @@
 import { getData } from "@/helpers";
 import Container from "./Container";
 import ProductData from "./ProductData";
+import { Products } from "../../type";
 
 const Products = async () => {
   const products = await getData();
 
   return (
     <Container className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 -mt-10">
-      {products?.map((product) => (
-        <ProductData />
+      {products?.map((product: Products) => (
+        <ProductData product={product} key={product._id} />
       ))}
     </Container>
   );
