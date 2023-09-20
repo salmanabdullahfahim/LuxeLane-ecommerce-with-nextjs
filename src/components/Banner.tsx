6f06e8e9d22ae6f8f78a2a1bd376b5a54149f750
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-20 17:22:32 +0600

    banner photo changed

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,6 +3,8 @@ import Slider from "react-slick";
 import banner1 from "@/images/bannerone.jpg";
 import banner2 from "@/images/bannertwo.jpg";
 import banner3 from "@/images/bannerthree.jpg";
+import banner4 from "@/images/handsome-adult-man-introduce-product-looking-pointing-fingers-left-promoting-something-against-b.jpg";
+import banner5 from "@/images/image-cheerful-girl-going-shopping-holding-bags-with-purchases-smiling-camera-standing-aga.jpg";
 import { PiCaretLeftLight, PiCaretRightLight } from "react-icons/pi";
 import Image from "next/image";
 import BannerText from "./BannerText";
@@ -44,19 +46,19 @@ const Banner = () => {
     <div className="relative">
       <Slider {...settings}>
         <div className="w-ful h-full relative">
-          <Image src={banner1} alt="banner 3" />
+          <Image src={banner2} alt="banner 3" />
           <BannerText title="Outware Picks" />
         </div>
         <div className="w-ful h-full relative">
-          <Image src={banner2} alt="banner2" />
+          <Image src={banner1} alt="banner2" />
           <BannerText title="Seasonal Offer" />
         </div>
         <div className="w-ful h-full relative">
-          <Image src={banner3} alt="banner 3" />
+          <Image src={banner4} alt="banner 3" />
           <BannerText title="Only for men" />
         </div>
       </Slider>
-      <div className="absolute w-full h-44 bg-gradient-to-t from-gray-100 to-transparent bottom-0 left-0 z-10" />
+      <div className="absolute w-full h-44 bg-gradient-to-t from-gray-50 to-transparent bottom-0 left-0 z-10" />
     </div>
   );
 };
