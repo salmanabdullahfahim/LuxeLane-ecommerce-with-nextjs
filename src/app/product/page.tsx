aec61c123e826bcc2067b9a3d5fad4b1b5140b91
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-22 08:20:30 +0600

    get the single product

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,8 +1,14 @@
+import { getSingleProduct } from "@/helpers";
+
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
 const ProductPage = ({ searchParams }: Props) => {
+  const idString = searchParams._id;
+  const _id = Number(idString);
+  const product = getSingleProduct(_id);
+  console.log(product);
   return <div>ProductPage</div>;
 };
 
