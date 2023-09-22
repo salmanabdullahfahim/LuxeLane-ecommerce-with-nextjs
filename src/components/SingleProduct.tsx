78e9fed4f8a7103490e2e3699758efeeb705f177
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-22 10:47:47 +0600

    add to cart button added in the single productPage

--- src/components/SingleProduct.tsx
+++ src/components/SingleProduct.tsx
@@ -1,9 +1,10 @@
 "use client";
 import Image from "next/image";
+import { IoMdCart } from "react-icons/io";
 
 const SingleProduct = ({ product }: any) => {
   return (
-    <div className="grid lg:grid-cols-2 gap-5 p-4 bg-gray-100 rounded-lg">
+    <div className="grid lg:grid-cols-2 gap-5 p-4 shadow-xl rounded-lg">
       <div>
         <Image
           src={product?.image}
@@ -13,8 +14,21 @@ const SingleProduct = ({ product }: any) => {
           className="w-full max-h-[700px] object-cover rounded-lg"
         />
       </div>
-      <div>
-        <p></p>
+      <div className="flex flex-col gap-y-7 justify-center">
+        <div>
+          <p className="text-3xl font-semibold">{product?.title}</p>
+          <p className="text-xl font-semibold">$ {product?.price}</p>
+        </div>
+        <p className="text-lightText">{product?.description}</p>
+        <div className="text-sm flex flex-col">
+          <span className="text-darkText">Product Code: {product?._id}</span>
+          <span className="text-darkText">Category: {product?.category}</span>
+        </div>
+        <div>
+          <button className="bg-darkText text-slate-100 uppercase text-sm py-3 px-3 flex items-center justify-center gap-x-1 hover:bg-black/70 rounded-sm">
+            Add to Cart <IoMdCart />
+          </button>
+        </div>
       </div>
     </div>
   );
