a3a7e86bd534c3793243d51643a847d7fc6aceab
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-22 08:33:56 +0600

    trendy product added

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,15 +1,31 @@
-import { getSingleProduct } from "@/helpers";
+import Container from "@/components/Container";
+import { getSingleProduct, getTrendingData } from "@/helpers";
+import { Products } from "../../../type";
+import ProductData from "@/components/ProductData";
 
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-const ProductPage = ({ searchParams }: Props) => {
+const ProductPage = async ({ searchParams }: Props) => {
   const idString = searchParams._id;
   const _id = Number(idString);
   const product = getSingleProduct(_id);
-  console.log(product);
-  return <div>ProductPage</div>;
+  const trendyProduct = await getTrendingData();
+  return (
+    <div>
+      <Container>
+        <div>
+          <p>Trending Product</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+            {trendyProduct.map((item: Products) => (
+              <ProductData key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductPage;
