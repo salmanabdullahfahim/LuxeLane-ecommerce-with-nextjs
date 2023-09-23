597553dab052f776eab2f1234cef93f65573c60b
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-23 20:30:55 +0600

    reset cart functionality added

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Container from "@/components/Container";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { stateProps } from "../../../type";
 import CartItem from "@/components/CartItem";
+import { resetCart } from "@/redux/shopSlice";
 
 const CartPage = () => {
   const { productData } = useSelector((state: stateProps) => state?.shop);
+  const dispatch = useDispatch();
 
   return (
     <Container>
@@ -15,7 +17,10 @@ const CartPage = () => {
         <CartItem />
 
         <div className="flex justify-end items-center">
-          <button className="bg-blue-500 text-slate-100 py-2 px-6 hover:bg-blue-600 hover:text-white duration-200 font-semibold rounded-md">
+          <button
+            onClick={() => dispatch(resetCart())}
+            className="bg-blue-500 text-slate-100 py-2 px-6 hover:bg-blue-600 hover:text-white duration-200 font-semibold rounded-md"
+          >
             Reset Cart
           </button>
         </div>
