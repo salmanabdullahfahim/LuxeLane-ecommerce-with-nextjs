5e05752e0810462b4c28b36be66657e3c80d2c21
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-23 20:27:14 +0600

    reset cart button added in the cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -11,7 +11,15 @@ const CartPage = () => {
   return (
     <Container>
       <h2 className="text-2xl mb-2 font-semibold">Cart</h2>
-      <CartItem />
+      <div className="flex flex-col gap-y-6">
+        <CartItem />
+
+        <div className="flex justify-end items-center">
+          <button className="bg-blue-500 text-slate-100 py-2 px-6 hover:bg-blue-600 hover:text-white duration-200 font-semibold rounded-md">
+            Reset Cart
+          </button>
+        </div>
+      </div>
     </Container>
   );
 };
