628051be271ae29bce39ff3949230affc2a2c0b5
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-24 08:06:37 +0600

    return to home show when no product in cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,6 +6,7 @@ import { stateProps } from "../../../type";
 import CartItem from "@/components/CartItem";
 import { resetCart } from "@/redux/shopSlice";
 import PaymentForm from "@/components/PaymentSection";
+import Link from "next/link";
 
 const CartPage = () => {
   const { productData } = useSelector((state: stateProps) => state?.shop);
@@ -13,20 +14,35 @@ const CartPage = () => {
 
   return (
     <Container>
-      <h2 className="text-2xl mb-2 font-semibold">Cart</h2>
-      <div className="flex flex-col gap-y-6">
-        <CartItem />
+      {productData.length > 0 ? (
+        <Container>
+          <h2 className="text-2xl mb-2 font-semibold">Cart</h2>
+          <div className="flex flex-col gap-y-6">
+            <CartItem />
 
-        <div className="flex justify-end items-center">
-          <button
-            onClick={() => dispatch(resetCart())}
-            className="bg-blue-500 text-slate-100 py-2 px-6 hover:bg-blue-600 hover:text-white duration-200 font-semibold rounded-md"
-          >
-            Reset Cart
-          </button>
+            <div className="flex justify-end items-center">
+              <button
+                onClick={() => dispatch(resetCart())}
+                className="bg-blue-500 text-slate-100 py-2 px-6 hover:bg-blue-600 hover:text-white duration-200 font-semibold rounded-md"
+              >
+                Reset Cart
+              </button>
+            </div>
+            <PaymentForm />
+          </div>
+        </Container>
+      ) : (
+        <div className="flex flex-col gap-y-6 items-center justify-center bg-white h-96 px-4">
+          <p className="border-[1px] border-blue-500 w-full p-2 text-center rounded-md">
+            Your product cart is empty
+          </p>
+          <Link href={"/"}>
+            <button className="bg-darkText text-white py-2 px-6 rounded-md hover:bg-black/75 duration-200">
+              Return to Home
+            </button>
+          </Link>
         </div>
-        <PaymentForm />
-      </div>
+      )}
     </Container>
   );
 };
