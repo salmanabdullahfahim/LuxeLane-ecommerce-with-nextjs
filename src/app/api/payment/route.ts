7238150c16345c56101ae390b0c612b2358c22de
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-25 19:55:16 +0600

    stripe payment added

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,13 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+import { Products } from "../../../../type";
 
 export const POST = async (request: NextRequest) => {
+  // @ts-ignore
+  const stripe = new Stripe(process.env.STRIPE_SECRET!);
   try {
     const requestBody = await request.json();
+    const { items, email } = await requestBody;
+
+    const extractingItems = await items.map((item: Products) => ({
+      quantity: item.quantity,
+      price_data: {
+        currency: "usd",
+        unit_amount: item.price * 100,
+        product_data: {
+          name: item.title,
+          description: item.description,
+          images: [item.image],
+        },
+      },
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: extractingItems,
+      mode: "payment",
+      success_url: `${process.env.NEXT_AUTH_URL}/success`,
+      cancel_url: `${process.env.NEXT_AUTH_URL}/checkout`,
+      metadata: {
+        email,
+      },
+    });
 
     return NextResponse.json({
       message: "Connecting to server",
       success: true,
-      requestBody,
+      id: session.id,
     });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
